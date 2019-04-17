91e201ffdf98aeba4d747c3f27e8d58fa2a68c26
Klaas Kelchtermans <kkelchte@opal.esat.kuleuven.be>
2019-04-17 10:04:36 +0200

    update:

--- pilot/models/__init__.py
+++ pilot/models/__init__.py
@@ -9,9 +9,10 @@ import dense_net
 import inception_net
 import mobile_net
 import tiny_LSTM_net
+import tinyv3_LSTM_net
 import alex_LSTM_net
 import tiny_nfc_net
 import tinyv3_nfc_net
 import tiny_3d_net
 import tinyv3_3d_net
-__all__ = ["tiny_net","tinyv3_net","tinyv4_net","res18_net","alex_net","vgg16_net","squeeze_net","dense_net","inception_net","mobile_net","tiny_LSTM_net","alex_LSTM_net","tiny_nfc_net","tiny_3d_net","tinyv3_nfc_net","tinyv3_3d_net"]
+__all__ = ["tiny_net","tinyv3_net","tinyv4_net","res18_net","alex_net","vgg16_net","squeeze_net","dense_net","inception_net","mobile_net","tiny_LSTM_net","tinyv3_LSTM_net","alex_LSTM_net","tiny_nfc_net","tiny_3d_net","tinyv3_nfc_net","tinyv3_3d_net"]
